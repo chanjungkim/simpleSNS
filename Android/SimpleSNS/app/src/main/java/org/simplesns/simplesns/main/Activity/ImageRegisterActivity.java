aa249c68e3084331e4048902dfe1bb9281bd6575
kys <ergla@naver.com>
2019-01-29 22:59:13 +0900

    피드 등록 페이지 생성
    페이지 생성

---
+++ Android/SimpleSNS/app/src/main/java/org/simplesns/simplesns/main/Activity/ImageRegisterActivity.java
@@ -0,0 +1,15 @@
+package org.simplesns.simplesns.main.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.simplesns.simplesns.R;
+
+public class ImageRegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_register);
+    }
+
+}
